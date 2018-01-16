4c9c39d4b6772fc1274ef13c6d7b608b1ce5bdab
user <user@DESKTOP-OAL422G>
2018-01-16 20:35:56 +0800

    Hello 2.0

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -17,5 +17,7 @@ public class HelloWorld {
         System.out.println("Hello World");
         System.out.println("Hello World");
         System.out.println("Hello World");
+          System.out.println("Hello World");
+        System.out.println("Hello World");
     }
 }
